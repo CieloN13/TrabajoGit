171af300131342ef9806629f85d46c6f58bf60f0
CieloN13 <cnmunar@soy.sena.edu.co>
2023-05-15 18:38:13 -0500

    Primer commit

---
+++ EjercicioGit.py
@@ -0,0 +1,10 @@
+"""Realice un algoritmo en donde se muestre un menú de 4 opciones, 
+en donde se permita seleccionar una opción. En la opción 1 se va a 
+calcular el área, perímetro y el tipo de triangulo de un triángulo, 
+en la opción 2 calcular el área, perímetro y Diagonal de un cuadrado, 
+en la tercera opción calcular el área, Diámetro y circunferencia 
+de un circulo y en la cuarta opción calcular el área, perímetro y Diagonal de rectángulo. 
+•	Triangulo: Área, perímetro y el tipo de triangulo
+•	Cuadrado: Área, perímetro y Diagonal 
+•	Circulo: Área, Diámetro y circunferencia
+•	Rectángulo: Área(Nicole), perímetro(Aleja) y Diagonal (Luis)."""
